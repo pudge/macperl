59a4e63977a9e77e1ee1e8fa75363d5c8a437d77
neeri <>
2000-08-06 09:30:00 +0000

    Importing perl 5.6.0

---
+++ perl/x2p/EXTERN.h
@@ -0,0 +1,17 @@
+/* $RCSfile$$Revision$$Date$
+ *
+ *    Copyright (c) 1991-1997, Larry Wall
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ * $Log$
+ */
+
+#undef EXT
+#define EXT extern
+
+#undef INIT
+#define INIT(x)
+
+#undef DOINIT
---
+++ perl/x2p/INTERN.h
@@ -0,0 +1,17 @@
+/* $RCSfile$$Revision$$Date$
+ *
+ *    Copyright (c) 1991-1997, Larry Wall
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ * $Log$
+ */
+
+#undef EXT
+#define EXT
+
+#undef INIT
+#define INIT(x) = x
+
+#define DOINIT
