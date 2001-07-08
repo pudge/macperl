ea81c7f87c8f1acc68bdb0d4a9debeac317f3f58
pudge <>
2001-07-08 05:07:14 +0000

    Version update 5.6.1a3

--- perl/macos/MPVersion.r
+++ perl/macos/MPVersion.r
@@ -5,6 +5,9 @@
  *    as specified in the README file.
  *
  * $Log$
+ * Revision 1.3  2001/05/05 20:32:41  pudge
+ * Prepare for 5.6.1a2, mostly updates to tests, and File::Find, and latest changes from main repository
+ *
  * Revision 1.2  2001/04/25 04:32:42  pudge
  * Update modules versions
  *
@@ -14,9 +17,9 @@
  *
  */
 
-#define MPDate		"2001-05-05"
-#define MPVersionStr	"5.6.1a2"
+#define MPDate		"2001-07-07"
+#define MPVersionStr	"5.6.1a3"
 #define MPRevision	0x05
 #define MPVersion	0x61
-#define MPBuild		0x02
+#define MPBuild		0x03
 #define MPState		alpha
